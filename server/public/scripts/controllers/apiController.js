195f015ec339353a08b140169b9f740fef6fd651
Adam Hefnider <ahefnider@yahoo.com>
2016-06-17 15:29:19 -0500

    changes to database

--- server/public/scripts/controllers/apiController.js
+++ server/public/scripts/controllers/apiController.js
@@ -3,7 +3,7 @@
 
 myApp.controller('APIController', ['$scope', '$http', function($scope, $http) {
   var key = 'cda6e4452910c4bd62fec694b9699db14588146d';
-  var baseURL = 'http://www.giantbomb.com/api/search/?api_key=';
+  var baseURL = 'https://www.giantbomb.com/api/search/?api_key=';
 
   $scope.getApiGame = function(wantedGame) {
     var query = key;
