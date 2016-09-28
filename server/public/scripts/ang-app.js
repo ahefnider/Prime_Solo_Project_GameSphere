0da640ed54d2b405c14767e551543f6d11abfe00
Adam Hefnider <ahefnider@yahoo.com>
2016-09-28 09:19:52 -0500

    none

--- server/public/scripts/ang-app.js
+++ server/public/scripts/ang-app.js
@@ -1,36 +1,36 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
 myApp.config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-    .when('/signinhome', {
-      templateUrl: '/views/signinhome.html',
-      controller: "LoginController"
-    })
-    .when('/register', {
-      templateUrl: '/views/register.html',
-      controller: "LoginController"
-    })
-    .when('/user', {
-      templateUrl: '/views/user.html',
-      controller: "UserController"
-    })
-    .when('/home', {
-      templateUrl: '/views/home.html',
-      controller: "HomeController"
-    })
-    .when('/library', {
-      templateUrl: '/views/library.html',
-      controller: "LibraryController"
-    })
-    .when('/random', {
-      templateUrl: '/views/random.html',
-      controller: "RandomController"
-    })
-    .when('/edit', {
-      templateUrl: '/views/edit.html',
-      controller: "EditController"
-    })
-    .otherwise({
-      redirectTo: 'signinhome'
-    })
+    $routeProvider
+        .when('/signinhome', {
+            templateUrl: '/views/signinhome.html',
+            controller: "LoginController"
+        })
+        .when('/register', {
+            templateUrl: '/views/register.html',
+            controller: "LoginController"
+        })
+        .when('/user', {
+            templateUrl: '/views/user.html',
+            controller: "UserController"
+        })
+        .when('/home', {
+            templateUrl: '/views/home.html',
+            controller: "HomeController"
+        })
+        .when('/library', {
+            templateUrl: '/views/library.html',
+            controller: "LibraryController"
+        })
+        .when('/random', {
+            templateUrl: '/views/random.html',
+            controller: "RandomController"
+        })
+        .when('/edit', {
+            templateUrl: '/views/edit.html',
+            controller: "EditController"
+        })
+        .otherwise({
+            redirectTo: 'signinhome'
+        })
 }]);
--- server/public/scripts/controllers/apiController.js
+++ server/public/scripts/controllers/apiController.js
@@ -1,60 +1,59 @@
-
 // Extra Stretch Goal: Include Giant Bombs API to show cover art and description of games in Library and Random page.
 
 myApp.controller('APIController', ['$scope', '$http', function($scope, $http) {
-  var key = 'cda6e4452910c4bd62fec694b9699db14588146d';
-  var baseURL = 'https://www.giantbomb.com/api/search/?api_key=';
+    var key = 'cda6e4452910c4bd62fec694b9699db14588146d';
+    var baseURL = 'https://www.giantbomb.com/api/search/?api_key=';
 
-  $scope.getApiGame = function(wantedGame) {
-    var query = key;
-    query += '&format=json&query="' + wantedGame + '"';
-    query += '&resources=game'
+    $scope.getApiGame = function(wantedGame) {
+        var query = key;
+        query += '&format=json&query="' + wantedGame + '"';
+        query += '&resources=game'
 
-    var request = baseURL + encodeURI(query);
+        var request = baseURL + encodeURI(query);
 
-    console.log(request);
-    if (wantedGame == undefined) {
-      alert("Enter a title.");
-    } else {
-    $http({
-        method: 'JSONP',
-        url: request,
-        params: {
-            api_key: key,
-            format: 'jsonp',
-            json_callback: 'JSON_CALLBACK'
+        console.log(request);
+        if (wantedGame == undefined) {
+            alert("Enter a title.");
+        } else {
+            $http({
+                method: 'JSONP',
+                url: request,
+                params: {
+                    api_key: key,
+                    format: 'jsonp',
+                    json_callback: 'JSON_CALLBACK'
+                }
+
+            }).then(function(data) {
+                $scope.data = data;
+                $scope.link = $scope.data.data.results[0].site_detail_url;
+                $scope.name = $scope.data.data.results[0].name;
+                var apiReleaseDate = $scope.data.data.results[0].original_release_date;
+                $scope.apiReleaseDate = new Date(apiReleaseDate);
+                $scope.apiDeck = $scope.data.data.results[0].deck;
+
+
+                // ********** THIS IS ME TRYING TO FIGURE OUT WHY IMAGES DON'T SHOW UP WHEN ON HEROKU *********
+
+
+                // $scope.apiImage = "http://static.giantbomb.com" + $scope.data.data.results[0].image.thumb_url;
+                $scope.apiImage = $scope.data.data.results[0].image.thumb_url;
+                // $scope.apiImage = $scope.data.data.results[0].image.thumb_url;
+
+
+
+                console.log($scope.data.data.results[0]);
+            });
         }
 
-    }).then(function (data) {
-        $scope.data = data;
-        $scope.link = $scope.data.data.results[0].site_detail_url;
-        $scope.name = $scope.data.data.results[0].name;
-        var apiReleaseDate = $scope.data.data.results[0].original_release_date;
-        $scope.apiReleaseDate = new Date(apiReleaseDate);
-        $scope.apiDeck = $scope.data.data.results[0].deck;
-
-
-        // ********** THIS IS ME TRYING TO FIGURE OUT WHY IMAGES DON'T SHOW UP WHEN ON HEROKU *********
-
-
-        // $scope.apiImage = "http://static.giantbomb.com" + $scope.data.data.results[0].image.thumb_url;
-        $scope.apiImage = $scope.data.data.results[0].image.thumb_url;
-        // $scope.apiImage = $scope.data.data.results[0].image.thumb_url;
-
-
-
-        console.log($scope.data.data.results[0]);
-    });
-}
-
-    $scope.showApiData = function () {
-      if ($scope.data == 'null') {
-        return false;
-      }else if ($scope.data == undefined) {
-        return false;
-      } else {
-        return true;
-      }
-    };
-  }
+        $scope.showApiData = function() {
+            if ($scope.data == 'null') {
+                return false;
+            } else if ($scope.data == undefined) {
+                return false;
+            } else {
+                return true;
+            }
+        };
+    }
 }]);
